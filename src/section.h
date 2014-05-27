16a0a5669dbac526bf108df409438be4041641fd
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-27 22:46:26 +0100

    Simplified ELF section data structure

---
+++ src/section.h
@@ -0,0 +1,16 @@
+#ifndef CTF_SECTION_H
+#define CTF_SECTION_H
+
+#include <stdlib.h>
+
+/**
+ * Simplified representation of the ELF section.
+ */
+struct ctf_section
+{
+	void *data; /**< section data */
+	size_t size; /**< section size in bytes */
+};
+
+#endif
+
