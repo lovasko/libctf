391b898765d3c14725c88ff635938db4b3f708af
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-28 14:55:39 +0100

    Enum element binary reflection

---
+++ src/enum.h
@@ -0,0 +1,19 @@
+#ifndef CTF_ENUM_H
+#define CTF_ENUM_H
+
+#include <stdint.h>
+
+/**
+ * Binary reflection of the enum variant data.
+ *
+ * Designated for internal use only.
+ */
+struct _ctf_enum
+{
+	uint32_t name; /**< name of the enum element */
+	int32_t value; /**< value associated with this element */
+};
+#define _CTF_ENUM sizeof(struct _ctf_enum)
+
+#endif
+
