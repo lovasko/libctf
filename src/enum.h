ddbbdae5642a0c738a749e3b79664666483d8cb0
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 03:23:25 +0100

    Renamed _ctf_enum to _ctf_enum_entry

--- src/enum.h
+++ src/enum.h
@@ -8,7 +8,7 @@
  *
  * Designated for internal use only.
  */
-struct _ctf_enum
+struct _ctf_enum_entry
 {
 	uint32_t name; /**< name of the enum element */
 	int32_t value; /**< value associated with this element */
