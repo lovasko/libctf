7d378465afac3b02f68c4e036ce9b0911d2d22a6
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 13:22:32 +0100

    Variable data parsing functions

---
+++ src/vardata.c
@@ -0,0 +1,117 @@
+#include "vardata.h"
+#include "array.h"
+#include "type.h"
+#include "enum.h"
+#include "function.h"
+#include "argument.h"
+#include "member.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+
+void*
+read_int_float_vardata (void *data)
+{
+	uint32_t *raw = (uint32_t*)data;
+	struct ctf_int_float_vardata *vardata = malloc(CTF_INT_FLOAT_VARDATA_SIZE);
+
+	vardata->encoding = (*raw & CTF_INT_FLOAT_ENCODING_MASK) >> 24; 
+	vardata->offset = (*raw & CTF_INT_FLOAT_OFFSET_MASK) >> 16;
+	vardata->size = *raw & CTF_INT_FLOAT_SIZE_MASK;
+
+	return (void*)vardata;
+}
+
+void*
+read_array_vardata (void *data, struct ctf_type **id_table)
+{
+	struct _ctf_array *raw = (struct _ctf_array*)data;
+	struct ctf_array *array = (struct ctf_array*)malloc(CTF_ARRAY_SIZE);
+
+	array->element_count = raw->element_count;
+	array->type = id_table[raw->content_type];
+
+	return (void*)array;
+}
+
+void*
+read_function_vardata (void *data, struct ctf_type **id_table, uint16_t length,
+    struct ctf_type *return_type)
+{
+	struct ctf_argument_head *argument_head = malloc(CTF_ARGUMENT_HEAD_SIZE);
+	LIST_INIT(argument_head);
+
+	uint16_t *raw_arguments = (uint16_t*)data;
+	for (uint16_t i = 0; i < length; i++)
+	{
+		struct ctf_argument *argument = malloc(CTF_ARGUMENT_SIZE);
+		argument->type = id_table[raw_arguments[i]];
+
+		LIST_INSERT_HEAD(argument_head, argument, arguments);
+	}
+
+	struct ctf_function *function = malloc(CTF_FUNCTION_SIZE);
+	function->return_type = return_type;
+	function->argument_head = argument_head;
+
+	return (void*)function;
+}
+
+void*
+read_enum_vardata (void *data, uint16_t length)
+{
+	struct ctf_enum_head *enum_head = malloc(CTF_ENUM_HEAD_SIZE);
+	LIST_INIT(enum_head);
+
+	struct _ctf_enum_entry *raw_enum_entries = (struct _ctf_enum_entry*)data;
+	for (uint16_t i = 0; i < length; i++)
+	{
+		struct ctf_enum_entry *enum_entry = malloc(CTF_ENUM_ENTRY_SIZE);
+		enum_entry->name = "enum_entry_name";
+		enum_entry->value = raw_enum_entries[i].value; 
+
+		LIST_INSERT_HEAD(enum_head, enum_entry, entries);
+	}
+
+	return (void*)enum_head;
+}
+
+void*
+read_struct_union_vardata (void *data, struct ctf_type **id_table, 
+    uint16_t length, uint16_t size)
+{
+	struct _ctf_small_member *small_member;
+	struct _ctf_large_member *large_member;
+	uint8_t member_type = 0;
+
+	if (size > CTF_MEMBER_THRESHOLD)
+	{
+		member_type = 1;
+		small_member = NULL;
+		large_member = data;
+	}
+	else
+	{
+		member_type = 0;
+		small_member = data;
+		large_member = NULL;
+	}
+
+	struct ctf_member_head *member_head = malloc(CTF_MEMBER_HEAD_SIZE);
+	LIST_INIT(member_head);
+
+	for (unsigned int i = 0; i < length; i++)
+	{
+		struct ctf_member *member = malloc(CTF_MEMBER_SIZE);
+		member->name = "member name";
+		member->type = id_table[(member_type ? large_member[i].offset :
+		    small_member[i].offset)];
+		member->offset = (member_type ? large_member[i].offset :
+		    small_member[i].offset);
+
+		LIST_INSERT_HEAD(member_head, member, members);
+	}
+
+	return (void*)member_head;
+}
+
---
+++ src/vardata.h
@@ -0,0 +1,40 @@
+#ifndef CTF_VARDATA_H
+#define CTF_VARDATA_H
+
+#include "type.h"
+
+#include <stdint.h>
+
+#define CTF_VARDATA_LENGTH_MAX 0x3ff
+
+#define CTF_INT_FLOAT_ENCODING_MASK 0xff000000
+#define CTF_INT_FLOAT_OFFSET_MASK   0x00ff0000 
+#define CTF_INT_FLOAT_SIZE_MASK     0x0000ffff 
+
+struct ctf_int_float_vardata
+{
+	uint8_t encoding;
+	uint8_t offset;
+	uint16_t size;
+};
+#define CTF_INT_FLOAT_VARDATA_SIZE sizeof(struct ctf_int_float_vardata)
+
+void*
+read_int_float_vardata (void *data);
+
+void*
+read_array_vardata (void *data, struct ctf_type **id_table);
+
+void*
+read_function_vardata (void *data, struct ctf_type **id_table, uint16_t length,
+    struct ctf_type *return_type);
+
+void*
+read_enum_vardata (void *data, uint16_t length);
+
+void*
+read_struct_union_vardata (void *data, struct ctf_type **id_table, 
+    uint16_t length, uint16_t size);
+
+#endif
+
