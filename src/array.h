c16f3e2b28b5c644c5719847700b54d70816a267
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 16:35:52 +0100

    Array documentation.

--- src/array.h
+++ src/array.h
@@ -17,10 +17,17 @@ struct _ctf_array
 };
 #define _CTF_ARRAY_SIZE sizeof(struct _ctf_array)
 
+/**
+ * Array vardata.
+ *
+ * We do not convert the index_type from the binary reflection. Name of the
+ * array is saved in the parent ctf_type, as this struct is stored in the data
+ * field of the ctf_type.
+ */
 struct ctf_array
 {
-	uint32_t element_count;
-	struct ctf_type *type;
+	uint32_t element_count; /**< element count */
+	struct ctf_type *type; /**< type of the array content */
 };
 #define CTF_ARRAY_SIZE sizeof(struct ctf_array)
 
