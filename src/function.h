29abea709cde966bd893f0bd0b3e49169fa4557d
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 16:39:25 +0100

    Function documentation.

--- src/function.h
+++ src/function.h
@@ -4,10 +4,13 @@
 #include "type.h"
 #include "argument.h"
 
+/**
+ * Function vardata.
+ */
 struct ctf_function
 {
-	struct ctf_type *return_type;
-	struct ctf_argument_head *argument_head;
+	struct ctf_type *return_type; /**< return type of the function */
+	struct ctf_argument_head *argument_head; /**< linked list of arguments */
 };
 #define CTF_FUNCTION_SIZE sizeof(struct ctf_function)
 
