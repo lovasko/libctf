e02d5ed83c4e6ca0be39bc4780865d14226b2b44
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 16:45:53 +0100

    Whitespace alignment of kind constants

--- src/kind.h
+++ src/kind.h
@@ -4,23 +4,23 @@
 #include <stdint.h>
 
 /* KIND constants that are contained in the type information */
-#define CTF_KIND_NONE 0
-#define CTF_KIND_INT 1
-#define CTF_KIND_FLOAT 2
-#define CTF_KIND_POINTER 3
-#define CTF_KIND_ARRAY 4
-#define CTF_KIND_FUNC 5
-#define CTF_KIND_STRUCT 6
-#define CTF_KIND_UNION 7
-#define CTF_KIND_ENUM 8
-#define CTF_KIND_FORWARD 9
-#define CTF_KIND_TYPEDEF 10
+#define CTF_KIND_NONE      0
+#define CTF_KIND_INT       1
+#define CTF_KIND_FLOAT     2
+#define CTF_KIND_POINTER   3
+#define CTF_KIND_ARRAY     4
+#define CTF_KIND_FUNC      5
+#define CTF_KIND_STRUCT    6
+#define CTF_KIND_UNION     7
+#define CTF_KIND_ENUM      8
+#define CTF_KIND_FORWARD   9
+#define CTF_KIND_TYPEDEF  10
 #define CTF_KIND_VOLATILE 11
-#define CTF_KIND_CONST 12
+#define CTF_KIND_CONST    12
 #define CTF_KIND_RESTRICT 13
 
 /* current maximum of KIND constants (all others should be <=) */
-#define CTF_KIND_MAX 13
+#define CTF_KIND_MAX      13
 
 /**
  * Extract the kind from a type information.
