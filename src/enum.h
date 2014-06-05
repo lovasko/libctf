3a91c0f8b25cd43bd58846970c3ef882ef001cfc
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-05 04:11:44 +0100

    User-consumable enum definition

--- src/enum.h
+++ src/enum.h
@@ -13,7 +13,19 @@ struct _ctf_enum_entry
 	uint32_t name; /**< name of the enum element */
 	int32_t value; /**< value associated with this element */
 };
-#define _CTF_ENUM sizeof(struct _ctf_enum)
+#define _CTF_ENUM_ENTRY_SIZE sizeof(struct _ctf_enum)
+
+struct ctf_enum_entry
+{
+	char *name;
+	int32_t value;
+
+	LIST_ENTRY(ctf_enum_entry) entries;
+};
+#define CTF_ENUM_ENTRY_SIZE sizeof(struct ctf_enum_entry)
+
+LIST_HEAD(ctf_enum_head, ctf_enum_entry);
+#define CTF_ENUM_HEAD_SIZE sizeof(struct ctf_enum_head)
 
 #endif
 
