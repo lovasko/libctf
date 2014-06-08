23580c3fc31b9b00fdbe68cfefa7fcc387f0f761
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-08 03:00:59 +0100

    ELF reading & decompression support

--- src/read.c
+++ src/read.c
@@ -1,14 +1,20 @@
 #include "read.h"
 #include "header.h"
-#include "type.h"
-#include "kind.h"
-#include "vardata.h"
-#include "array.h"
-#include "member.h"
-#include "enum.h"
+#include "util/compression.h"
+#include "util/elf_section_find.h"
+#include "strings.h"
 
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/queue.h>
+#include <fcntl.h>
+#include <libelf.h>
+#include <gelf.h>
+#include <stdint.h>
 
 char *ctf_string_table;
 char *elf_string_table;
@@ -40,36 +46,114 @@ header_preface_check (struct _ctf_preface *preface)
 struct ctf_file*
 ctf_read_file (char *filename)
 {
-	struct _ctf_header header;
-	memcpy(&header, ctf.data, _CTF_HEADER_SIZE);
-
-	if (header_preface_check(&header.preface) != 0)
+	/* open the file */
+	int fd;
+	if ((fd = open(filename, O_RDONLY)) < 0)
 		return NULL;
 
-	ctf_string_table = ctf.data + _CTF_HEADER_SIZE + header.string_offset;
-	elf_string_table = elf_strtab.data;
+	/* read the ELF header */
+	Elf32_Ehdr elf_header;
+	if (read(fd, &elf_header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+	{
+		close(fd);
+		return NULL;
+	}
 
-	struct ctf_section label_section;
-	label_section.data = ctf.data + _CTF_HEADER_SIZE + header.label_offset;
-	label_section.size = header.object_offset - header.label_offset;
+	/* set the libelf version */
+	if (elf_version(EV_CURRENT) == EV_NONE)
+	{
+		close(fd);
+		return NULL;
+	}
 
+	/* load the ELF file */
+	Elf *elf = NULL;
+	if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
+	{
+		printf("ELF ERROR: %s\n", elf_errmsg(elf_errno()));	
+		close(fd);
+		return NULL;
+	}
+
+	/* find the CTF section */
+	struct _section *ctf_section = elf_section_find(elf, &elf_header,
+	    CTF_ELF_SECTION_SUNW_CTF);
+	if (ctf_section == NULL)
+		return NULL;
+
+	/* find the string table section */
+	struct _section *strtab_section = elf_section_find(elf, &elf_header,
+	    CTF_ELF_SECTION_STRTAB);
+	if (strtab_section == NULL)
+		return NULL;
+
+	/* find the symbol table section */
+	struct _section *symtab_section = elf_section_find(elf, &elf_header,
+	    CTF_ELF_SECTION_SYMTAB);
+	if (symtab_section == NULL)
+		return NULL;
+
+	/* we do not need the ELF data anymore */
+	elf_end(elf);
+
+	/* read the CTF header */
+	struct _ctf_header *header = (struct _ctf_header*)ctf_section->data;
+	if (header_preface_check(&header->preface) != 0)
+		return NULL;
+
+	/* pointer to decompressed start of the actual CTF data without the header */
+	void *headerless_ctf;
+
+	/* decompress the CTF data (if compressed) */
+	if (header->preface.flags & CTF_COMPRESSED)
+	{
+		struct _section compressed;
+		compressed.data = ctf_section->data + _CTF_HEADER_SIZE;
+		compressed.size = ctf_section->size - _CTF_HEADER_SIZE;
+
+		struct _section *decompressed = decompress(&compressed);	
+		if (decompressed == NULL)
+			return NULL;
+
+		headerless_ctf = decompressed->data;
+	}
+	else
+		headerless_ctf = ctf_section->data + _CTF_HEADER_SIZE;
+
+	/* construct the string table data structure */
+	struct _strings strings;
+	strings.ctf = malloc(_SECTION_SIZE);
+	strings.ctf->data = headerless_ctf + header->string_offset;
+	strings.ctf->size = header->string_length;
+	strings.elf = strtab_section;
+
+	/* read the labels */
+	struct _section label_section;
+	label_section.data = headerless_ctf + header->label_offset;
+	label_section.size = header->object_offset - header->label_offset;
 	struct ctf_label_head label_head;	
 	LIST_INIT(&label_head);
-	read_labels(&label_head, &label_section);
+	read_labels(&label_head, &label_section, &strings);
 
-	struct ctf_section type_section;
-	type_section.data = ctf.data + _CTF_HEADER_SIZE + header.type_offset;
-	type_section.size = header.string_offset - header.type_offset;
-
+	/* read the types */
+	struct _section type_section;
+	type_section.data = headerless_ctf + header->type_offset;
+	type_section.size = header->string_offset - header->type_offset;
 	struct ctf_type_head type_head;	
 	LIST_INIT(&type_head);
-	read_types(&type_head, &type_section);
+	read_types(&type_head, &type_section, &strings);
 
 	struct ctf_file *file = malloc(CTF_FILE_SIZE);
 	file->label_head = label_head;
 	file->type_head = type_head;
 	file->version = CTF_VERSION;
-	file->compressed = 0;
+	file->compressed = header->preface.flags & CTF_COMPRESSED;
+
+	free(ctf_section);
+	free(strtab_section);
+	free(symtab_section);
+	free(strings.ctf);
+	close(fd);
 
 	return file;
 }
