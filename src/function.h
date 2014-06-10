a01debc2448abe4248d62e8fa5fccab2481f0f32
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-10 13:14:32 +0100

    Extended the function struct
    From now on, it is used to store two types of function occurrences: one from
    the type section and one from the function section. When the ctf_function comes
    from the type section, the name and functions are not filled in.

--- src/function.h
+++ src/function.h
@@ -4,19 +4,27 @@
 #include "type.h"
 #include "argument.h"
 
+#include <sys/queue.h>
+
 /**
  * Function vardata.
  */
 struct ctf_function
 {
+	char *name;
 	union
 	{
 		struct ctf_type *return_type; /**< return type of the function */
 		uint16_t return_type_reference;
 	};
 	struct ctf_argument_head *argument_head; /**< linked list of arguments */
+
+	LIST_ENTRY(ctf_function) functions;
 };
 #define CTF_FUNCTION_SIZE sizeof(struct ctf_function)
+
+LIST_HEAD(ctf_function_head, ctf_function);
+#define CTF_FUNCTION_HEAD_SIZE sizeof(struct ctf_function_head)
 
 #endif
 
