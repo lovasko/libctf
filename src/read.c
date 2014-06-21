cffcc23128d6d916353d7449cde46818b8a6d457
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-21 17:49:24 +0100

    Moved the elf_section_find function to the read.c and removed its dedicated file

--- src/read.c
+++ src/read.c
@@ -22,6 +22,34 @@
 #include <gelf.h>
 #include <stdint.h>
 
+static struct _section*
+elf_section_find (Elf *elf, Elf32_Ehdr *elf_header, const char *to_find)
+{
+	Elf_Scn *section = NULL;  	
+	GElf_Shdr section_header;
+	
+	while ((section = elf_nextscn(elf, section)) != 0)
+	{
+		gelf_getshdr(section, &section_header);
+		char *section_name = elf_strptr(elf, elf_header->e_shstrndx, 
+		    section_header.sh_name);
+
+		if (strcmp(section_name, to_find) == 0)
+		{
+			Elf_Data *data = elf_getdata(section, NULL);
+
+			struct _section *result = malloc(_SECTION_SIZE);
+			result->size = data->d_size;
+			result->data = malloc(data->d_size);
+			memcpy(result->data, data->d_buf, data->d_size);
+
+			return result;
+		}
+	}
+
+	return NULL;
+}
+
 static int
 header_preface_check (struct _ctf_preface *preface)
 {
