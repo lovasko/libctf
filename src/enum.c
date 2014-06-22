e3491516d7b94d45346df5777574ce729ddebc7e
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-22 18:48:26 +0100

    Finished public API for the enum and enum_entry

--- src/enum.c
+++ src/enum.c
@@ -1,24 +1,31 @@
 #include "enum.h"
+#include "errors.h"
 
 #include <stdint.h>
 #include <stdlib.h>
 
-char*
-ctf_enum_entry_get_name (struct ctf_enum_entry *enum_entry)
+int
+ctf_enum_entry_get_name (struct ctf_enum_entry *enum_entry, char **out_name)
 {
 	if (enum_entry)
-		return enum_entry->name;
+	{
+		*out_name = enum_entry->name;
+		return CTF_OK;
+	}
 	else
-		return NULL;
+		return CTF_E_NULL;
 }
 
-int32_t
-ctf_enum_entry_get_value (struct ctf_enum_entry *enum_entry)
+int
+ctf_enum_entry_get_value (struct ctf_enum_entry *enum_entry, int32_t **out_value)
 {
 	if (enum_entry)
-		return enum_entry->value;
+	{
+		*out_value = enum_entry->value;
+		return CTF_OK;
+	}
 	else
-		return 0;
+		return CTF_E_NULL;
 }
 
 int
--- src/enum.h
+++ src/enum.h
@@ -45,11 +45,12 @@ int
 ctf_enum_get_next_entry (struct ctf_enum *_enum, 
     struct ctf_enum_entry *enum_entry, struct ctf_enum_entry **out_enum_entry);
 
-char*
-ctf_enum_entry_get_name (struct ctf_enum_entry *enum_entry);
+int
+ctf_enum_entry_get_name (struct ctf_enum_entry *enum_entry, char **out_name);
 
-int32_t
-ctf_enum_entry_get_value (struct ctf_enum_entry *enum_entry);
+int
+ctf_enum_entry_get_value (struct ctf_enum_entry *enum_entry, 
+    int32_t **out_value);
 
 #endif
 
