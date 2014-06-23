9cf47a30419f9d35be45f22148be936102394a47
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-23 07:55:42 +0100

    More NULL checks

--- src/enum_entry.c
+++ src/enum_entry.c
@@ -7,7 +7,7 @@
 int
 ctf_enum_entry_get_name (struct ctf_enum_entry *enum_entry, char **out_name)
 {
-	if (enum_entry)
+	if (enum_entry && out_name)
 	{
 		*out_name = enum_entry->name;
 		return CTF_OK;
@@ -20,7 +20,7 @@ int
 ctf_enum_entry_get_value (struct ctf_enum_entry *enum_entry, 
     int32_t **out_value)
 {
-	if (enum_entry)
+	if (enum_entry && out_value)
 	{
 		*out_value = enum_entry->value;
 		return CTF_OK;
