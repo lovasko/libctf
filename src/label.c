4b47d5982336c16bc1a614b6b6044d6523747cd2
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-23 14:17:51 +0100

    Temporarily removed the API to add and remove the labels due to it being obsolete

--- src/label.c
+++ src/label.c
@@ -6,30 +6,6 @@
 #include <string.h>
 #include <sys/queue.h>
 
-/* TODO check for the duplicates based on the name */
-int
-ctf_label_add (struct ctf_label_head *head, struct ctf_label *to_add)
-{
-	TAILQ_INSERT_TAIL(head, to_add, labels);
-	return CTF_OK;
-}
-
-int
-ctf_label_remove (struct ctf_label_head *head, struct ctf_label *to_remove)
-{
-	struct ctf_label *runner;
-	TAILQ_FOREACH (runner, head, labels)
-	{
-		if (strcmp(runner->name, to_remove->name) == 0)
-		{
-			TAILQ_REMOVE(head, runner, labels);
-			return CTF_OK;
-		}
-	}
-
-	return CTF_E_NOT_FOUND;
-}
-
 char*
 ctf_label_get_name (struct ctf_label *label)
 {
--- src/label.h
+++ src/label.h
@@ -35,32 +35,6 @@ struct ctf_label
 TAILQ_HEAD(ctf_label_head, ctf_label);
 #define CTF_LABEL_HEAD_SIZE sizeof(struct ctf_label_head)
 
-/**
- * Add new label to the end of the list.
- *
- * This function does not make copy of the to_add parameter. Be sure to keep
- * the memory available during the lifetime of the list.
- *
- * @param head pointer to the head of the linked list (or any node in fact)
- * @param to_add label to be added
- * @return 0 on success, 1 otherwise
- */
-int
-ctf_label_add (struct ctf_label_head *head, struct ctf_label *to_add);
-
-/**
- * Remove the specified label from the list.
- *
- * Note that only the name of the label is being matched, since it is the
- * member variable that needs to be unique. 
- *
- * @param head pointer to the head of the linked list (or any node in fact)
- * @param to_remove label to be removed 
- * @return 0 on success, 1 otherwise
- */
-int
-ctf_label_remove (struct ctf_label_head *head, struct ctf_label *to_remove);
-
 char*
 ctf_label_get_name (struct ctf_label *label);
 
