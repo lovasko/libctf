1e5bb8c57bb9404756be67bf849d86b8e12b9523
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-29 14:12:49 +0100

    Adapted the function to new listing and property macros

--- src/function.c
+++ src/function.c
@@ -1,53 +1,9 @@
 #include "function.h"
-#include "errors.h"
 
-int
-ctf_function_get_name (struct ctf_function *function, char **out_name)
-{
-	if (function && out_name)
-	{
-		*out_name = function->name;
-		return CTF_OK;
-	}
-	else
-		return CTF_E_NULL;
-}
-
-int
-ctf_function_get_return_type (struct ctf_function *function, 
-    struct ctf_type **out_return_type)
-{
-	if (function && out_return_type)
-	{
-		*out_return_type = function->return_type;
-		return CTF_OK;
-	}
-	else
-		return CTF_E_NULL;
-}
-
-int
-ctf_function_get_next_argument (struct ctf_function *function, 
-    struct ctf_argument *argument, struct ctf_argument **out_argument)
-{
-	if (argument == NULL)
-	{
-		if (TAILQ_EMPTY(function->argument_head))
-			return CTF_EMPTY;
-
-		*out_argument = TAILQ_FIRST(function->argument_head);
-		return CTF_OK;
-	}
-	else
-	{
-		struct ctf_argument *next = TAILQ_NEXT(argument, arguments);
-		if (next == NULL)
-			return CTF_END;
-		else
-		{
-			*out_argument = next;
-			return CTF_OK;	
-		}
-	}
-}
+_CTF_GET_PROPERTY_IMPL(ctf_function_get_name, struct ctf_function*, char*, name)
+_CTF_GET_PROPERTY_IMPL(ctf_function_get_return_type, struct ctf_function*, 
+    struct ctf_type*, return_type)
+	
+_CTF_LISTING_IMPL(ctf_function_get_next_argument, struct ctf_function*,
+    struct ctf_argument*, argument_head, arguments);
 
--- src/function.h
+++ src/function.h
@@ -3,6 +3,8 @@
 
 #include "type.h"
 #include "argument.h"
+#include "util/property.h"
+#include "util/listing.h"
 
 #include <stdint.h>
 #include <sys/queue.h>
@@ -27,16 +29,12 @@ struct ctf_function
 TAILQ_HEAD(ctf_function_head, ctf_function);
 #define CTF_FUNCTION_HEAD_SIZE sizeof(struct ctf_function_head)
 
-int
-ctf_function_get_name (struct ctf_function *function, char **out_name);
-
-int
-ctf_function_get_return_type (struct ctf_function *function, 
-    struct ctf_type **out_return_type);
-
-int
-ctf_function_get_next_argument (struct ctf_function *function, 
-    struct ctf_argument *argument, struct ctf_argument **out_argument);
+_CTF_GET_PROPERTY_PROTO(ctf_function_get_name, struct ctf_function*, char*)
+_CTF_GET_PROPERTY_PROTO(ctf_function_get_return_type, struct ctf_function*, 
+    struct ctf_type*)
+	
+_CTF_LISTING_PROTO(ctf_function_get_next_argument, struct ctf_function*,
+    struct ctf_argument*);
 
 #endif
 
