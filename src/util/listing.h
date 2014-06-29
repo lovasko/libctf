87eaeebb7747dfa3e3cd89b5799a6ab0a962b124
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-29 13:07:29 +0100

    Fixed type errors

--- src/util/listing.h
+++ src/util/listing.h
@@ -1,21 +1,23 @@
 #include "errors.h"
 
+#include <stdlib.h>
+
 #define _CTF_LISTING_PROTO(FUNCTION_NAME, OBJECT_TYPE, ELEMENT_TYPE) \
 int \
-FUNCTION_NAME (OBJECT_TYPE* object, ELEMENT_TYPE* current, ELEMENT_TYPE** \
+FUNCTION_NAME (OBJECT_TYPE object, ELEMENT_TYPE current, ELEMENT_TYPE* \
 out_next);
 
 #define _CTF_LISTING_IMPL(FUNCTION_NAME, OBJECT_TYPE, ELEMENT_TYPE, HEAD_NAME, \
     NEXT_NAME) \
 int \
-FUNCTION_NAME (OBJECT_TYPE* object, ELEMENT_TYPE* current, ELEMENT_TYPE** \
+FUNCTION_NAME (OBJECT_TYPE object, ELEMENT_TYPE current, ELEMENT_TYPE* \
 out_next) \
 {                                                          \
 	if (object && out_next)                                  \
 	{                                                        \
 		if (current)                                           \
 		{                                                      \
-			ELEMENT_TYPE *next = TAILQ_NEXT(current, NEXT_NAME); \
+			ELEMENT_TYPE next = TAILQ_NEXT(current, NEXT_NAME); \
 			if (next)                                            \
 			{                                                    \
 				*out_next = next;                                  \
