8741ece4b15a22d21bbc163a72f912106b52aae0
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-06-29 05:45:52 +0100

    Whitespace fix

--- src/util/property.h
+++ src/util/property.h
@@ -2,20 +2,20 @@
 
 #define _CTF_GET_PROPERTY_PROTO(FUNCTION_NAME, OBJECT_TYPE, PROPERTY_TYPE, \
     PROPERTY_NAME) \
-int\
+int \
 FUNCTION_NAME (OBJECT_TYPE* object, PROPERTY_TYPE* out_property);
 
 #define _CTF_GET_PROPERTY_IMPL(FUNCTION_NAME, OBJECT_TYPE, PROPERTY_TYPE, \
     PROPERTY_NAME) \
-int\
+int \
 FUNCTION_NAME (OBJECT_TYPE* object, PROPERTY_TYPE* out_property) \
-{ \
-	if (object && out_property) \
-	{ \
+{                                          \
+	if (object && out_property)              \
+	{                                        \
 		*out_property = object->PROPERTY_NAME; \
-		return CTF_OK; \
-	} \
-	else \
-		return CTF_E_NULL; \
+		return CTF_OK;                         \
+	}                                        \
+	else                                     \
+		return CTF_E_NULL;                     \
 }
 
