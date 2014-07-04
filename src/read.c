412ecc3c0610ff41eb867fc2fd78518f2dc60ca3
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-04 19:26:49 +0100

    Use of the variable length extraction function

--- src/read.c
+++ src/read.c
@@ -312,7 +312,7 @@ read_types (struct ctf_file *file, struct _section *section, struct
 
 		struct _ctf_small_type *small_type = section->data + offset;	
 		uint8_t kind = ctf_kind_from_info(small_type->info);
-		uint16_t vardata_length = small_type->info & CTF_VARDATA_LENGTH_MAX;
+		uint16_t vardata_length = ctf_vlen_from_info(small_type->info);
 		uint8_t is_root = ctf_is_root_from_info(small_type->info);
 
 		struct ctf_type *type = malloc(CTF_TYPE_SIZE);			
