ee0c6d5b7104ecbb63ad5fead1e857ba6e3cdb71
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-09 00:58:14 +0100

    Moved the comment block to correct place

--- src/read/types.c
+++ src/read/types.c
@@ -106,6 +106,31 @@ create_type_table (struct ctf_file* file)
 	return CTF_OK;
 }
 
+/* TODO update this algorithm with newer knowledge!
+ * 
+ * Algorithm
+ *
+ * Assume that the type under the pointer is small_type. Examine the kind of
+ * the type: if it is pure reference kind = POINTER, TYPEDEF, VOLATILE, CONST,
+ * RESTRICT or one of the special kinds = FORWARD or NONE, there is no need to
+ * bring the large_type to the game. The data member of the ctf_type struct
+ * will simply point to another ctf_type. This pointer will be acquired from
+ * the "ID to pointer table". Each type that is read gets an incrementing
+ * unique integer ID. Therefore, if e.g. a TYPEDEF says that the reference type
+ * is 15, we need to look into our "ID to pointer table" at index 15 and just
+ * return the pointer. This introduces potential problem: what if there is a
+ * forward reference? For now, we hope there is no such thing. Future
+ * implementation might put these types aside, still recording them into the
+ * "ID to pointer table" and create some type of waiting queue that awaits its
+ * forward reference/base type.
+ * In case of other, more complex types INT, FLOAT, ARRAY, FUNC, STRUCT, UNION
+ * or ENUM, we need to check the size member (the type has no meaningful value)
+ * and decide, based on the comparison with the CTF_TYPE_THRESHOLD, whether to
+ * use the large_type. Afterwards, we need to read the variant data following
+ * the type data. This, as the name suggests, varies for every kind - we need a
+ * special function for every kind.
+ */
+
 int
 _ctf_read_types (struct ctf_file* file, struct _section* section, 
     struct _strings* strings)
--- src/type.c
+++ src/type.c
@@ -1,30 +1,5 @@
 #include "type.h"
 
-/*
- * TODO what is this doing here?
- * Algorithm
- *
- * Assume that the type under the pointer is small_type. Examine the kind of
- * the type: if it is pure reference kind = POINTER, TYPEDEF, VOLATILE, CONST,
- * RESTRICT or one of the special kinds = FORWARD or NONE, there is no need to
- * bring the large_type to the game. The data member of the ctf_type struct
- * will simply point to another ctf_type. This pointer will be acquired from
- * the "ID to pointer table". Each type that is read gets an incrementing
- * unique integer ID. Therefore, if e.g. a TYPEDEF says that the reference type
- * is 15, we need to look into our "ID to pointer table" at index 15 and just
- * return the pointer. This introduces potential problem: what if there is a
- * forward reference? For now, we hope there is no such thing. Future
- * implementation might put these types aside, still recording them into the
- * "ID to pointer table" and create some type of waiting queue that awaits its
- * forward reference/base type.
- * In case of other, more complex types INT, FLOAT, ARRAY, FUNC, STRUCT, UNION
- * or ENUM, we need to check the size member (the type has no meaningful value)
- * and decide, based on the comparison with the CTF_TYPE_THRESHOLD, whether to
- * use the large_type. Afterwards, we need to read the variant data following
- * the type data. This, as the name suggests, varies for every kind - we need a
- * special function for every kind.
- */
-
 _CTF_GET_PROPERTY_IMPL(ctf_type_get_kind, ctf_type, ctf_kind, kind)
 _CTF_GET_PROPERTY_IMPL(ctf_type_get_id, ctf_type, ctf_id, id)
 _CTF_GET_PROPERTY_IMPL(ctf_type_is_root, ctf_type, ctf_bool, is_root)
