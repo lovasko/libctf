9f50082701e829e956d0d8cb699507f4cdff1a68
Daniel Lovasko <lovasko@freebsd.org>
2014-07-12 00:26:10 +0200

    Typedef of the forward declaration

--- src/fwd_decl.h
+++ src/fwd_decl.h
@@ -13,6 +13,8 @@ struct ctf_fwd_decl
 };
 #define CTF_FWD_DECL_SIZE sizeof(struct ctf_fwd_decl)
 
+typedef struct ctf_fwd_decl* ctf_fwd_decl;
+
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_name, struct ctf_fwd_decl*, char*)
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_kind, struct ctf_fwd_decl*, uint8_t)
 
