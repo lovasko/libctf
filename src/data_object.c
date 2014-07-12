73ffc75f0645272b87fa40f09827c5263934bc97
Daniel Lovasko <lovasko@freebsd.org>
2014-07-12 10:30:29 +0200

    Properties update with new typedefs

--- src/data_object.c
+++ src/data_object.c
@@ -1,7 +1,6 @@
 #include "data_object.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_name, struct ctf_data_object*, char*,
-    name)
-_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_type, struct ctf_data_object*,
-    struct ctf_type*, type)
+_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_name, ctf_data_object, char*, name)
+_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_type, ctf_data_object, ctf_type, 
+    type)
 
--- src/data_object.h
+++ src/data_object.h
@@ -21,10 +21,8 @@ typedef struct ctf_data_object* ctf_data_object;
 TAILQ_HEAD(ctf_data_object_head, ctf_data_object);
 #define CTF_DATA_OBJECT_HEAD_SIZE sizeof(struct ctf_data_object_head)
 
-_CTF_GET_PROPERTY_PROTO(ctf_data_object_get_name, struct ctf_data_object*, 
-    char*)
-_CTF_GET_PROPERTY_PROTO(ctf_data_object_get_type, struct ctf_data_object*,
-    struct ctf_type*)
+_CTF_GET_PROPERTY_PROTO(ctf_data_object_get_name, ctf_data_object, char*)
+_CTF_GET_PROPERTY_PROTO(ctf_data_object_get_type, ctf_data_object, ctf_type)
 
 #endif
 
