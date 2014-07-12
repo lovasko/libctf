184c5b92c11e69f34c8cc82a0b3d95344cea41da
Daniel Lovasko <lovasko@freebsd.org>
2014-07-12 17:37:49 +0200

    Adapted properties and listing to new typedefs

--- src/function.c
+++ src/function.c
@@ -1,9 +1,9 @@
 #include "function.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_function_get_name, struct ctf_function*, char*, name)
-_CTF_GET_PROPERTY_IMPL(ctf_function_get_return_type, struct ctf_function*, 
-    struct ctf_type*, return_type)
+_CTF_GET_PROPERTY_IMPL(ctf_function_get_name, ctf_function, char*, name)
+_CTF_GET_PROPERTY_IMPL(ctf_function_get_return_type, ctf_function, ctf_type,
+    return_type)
 	
-_CTF_LISTING_IMPL(ctf_function_get_next_argument, struct ctf_function*,
-    struct ctf_argument*, argument_head, arguments);
+_CTF_LISTING_IMPL(ctf_function_get_next_argument, ctf_function, 
+    ctf_argument, argument_head, arguments)
 
--- src/function.h
+++ src/function.h
@@ -31,12 +31,10 @@ typedef struct ctf_function* ctf_function;
 TAILQ_HEAD(ctf_function_head, ctf_function);
 #define CTF_FUNCTION_HEAD_SIZE sizeof(struct ctf_function_head)
 
-_CTF_GET_PROPERTY_PROTO(ctf_function_get_name, struct ctf_function*, char*)
-_CTF_GET_PROPERTY_PROTO(ctf_function_get_return_type, struct ctf_function*, 
-    struct ctf_type*)
+_CTF_GET_PROPERTY_PROTO(ctf_function_get_name, ctf_function, char*)
+_CTF_GET_PROPERTY_PROTO(ctf_function_get_return_type, ctf_function, ctf_type)
 	
-_CTF_LISTING_PROTO(ctf_function_get_next_argument, struct ctf_function*,
-    struct ctf_argument*);
+_CTF_LISTING_PROTO(ctf_function_get_next_argument, ctf_function, ctf_argument)
 
 #endif
 
