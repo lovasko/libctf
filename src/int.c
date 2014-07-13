c2ff0c1922d35f2c0c40fe420c6e3005a267786a
Daniel Lovasko <lovasko@freebsd.org>
2014-07-13 16:46:38 +0200

    Changed stdint types with appropriate typedefs

--- src/int.c
+++ src/int.c
@@ -1,8 +1,8 @@
 #include "int.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_name, struct ctf_int*, char*, name)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_size, struct ctf_int*, uint16_t, size)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_offset, struct ctf_int*, uint8_t, offset)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_content, struct ctf_int*, uint8_t, content)
-_CTF_GET_PROPERTY_IMPL(ctf_int_is_signed, struct ctf_int*, int, is_signed)
+_CTF_GET_PROPERTY_IMPL(ctf_int_get_name, ctf_int, char*, name)
+_CTF_GET_PROPERTY_IMPL(ctf_int_get_size, ctf_int, ctf_int_size, size)
+_CTF_GET_PROPERTY_IMPL(ctf_int_get_offset, ctf_int, ctf_int_offset, offset)
+_CTF_GET_PROPERTY_IMPL(ctf_int_get_content, ctf_int, ctf_int_content, content)
+_CTF_GET_PROPERTY_IMPL(ctf_int_is_signed, ctf_int, ctf_bool, is_signed)
 
--- src/int.h
+++ src/int.h
@@ -1,30 +1,42 @@
 #ifndef CTF_INT_H
 #define CTF_INT_H
 
+#include "util/bool.h"
 #include "util/property.h"
 
 #include <stdint.h>
 
+typedef uint8_t ctf_int_content;
 #define CTF_INT_CONTENT_NUMBER  1
 #define CTF_INT_CONTENT_CHAR    2
 #define CTF_INT_CONTENT_BOOLEAN 3
 #define CTF_INT_CONTENT_VARARGS 4
 
+typedef uint8_t ctf_int_offset;
+#define CTF_INT_OFFSET_MIN   0
+#define CTF_INT_OFFSET_MAX 255
+
+typedef uint16_t ctf_int_size;
+#define CTF_INT_SIZE_MIN     0
+#define CTF_INT_SIZE_MAX 65535
+
 struct ctf_int
 {
-	char *name;
-	uint8_t content;
-	uint8_t offset;
-	uint16_t size;
-	int is_signed;
+	char* name;
+	ctf_int_content content;
+	ctf_int_offset offset;
+	ctf_int_size size;
+	ctf_bool is_signed;
 };
 #define CTF_INT_SIZE sizeof(struct ctf_int)
 
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_name, struct ctf_int*, char*)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_size, struct ctf_int*, uint16_t)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_offset, struct ctf_int*, uint8_t)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_content, struct ctf_int*, uint8_t)
-_CTF_GET_PROPERTY_PROTO(ctf_int_is_signed, struct ctf_int*, int)
+typedef struct ctf_int* ctf_int;
+
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_name, ctf_int, char*)
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_size, ctf_int, ctf_int_size)
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_offset, ctf_int, ctf_int_offset)
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_content, ctf_int, ctf_int_content)
+_CTF_GET_PROPERTY_PROTO(ctf_int_is_signed, ctf_int, ctf_bool)
 
 #endif
 
