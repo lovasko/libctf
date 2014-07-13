b629d9ce6268723e91852fb203963c7f9091d035
Daniel Lovasko <lovasko@freebsd.org>
2014-07-13 16:55:12 +0200

    Changed stdint types with appropriate typedefs

--- src/struct_union.c
+++ src/struct_union.c
@@ -1,8 +1,8 @@
 #include "struct_union.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_name, struct ctf_struct_union*,
-    char*, name)
+_CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_name, ctf_struct_union, char*, 
+    name)
 
-_CTF_LISTING_IMPL(ctf_struct_union_get_next_member, struct ctf_struct_union*,
-    struct ctf_member*, member_head, members)
+_CTF_LISTING_IMPL(ctf_struct_union_get_next_member, ctf_struct_union,
+    ctf_member, member_head, members)
 
--- src/struct_union.h
+++ src/struct_union.h
@@ -7,16 +7,17 @@
 
 struct ctf_struct_union
 {
-	char *name;
-	struct ctf_member_head *member_head;
+	char* name;
+	struct ctf_member_head* member_head;
 };
 #define CTF_STRUCT_UNION_SIZE sizeof(struct ctf_struct_union)
 
-_CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_name, struct ctf_struct_union*,
-    char*)
+typedef struct ctf_struct_union* ctf_struct_union;
 
-_CTF_LISTING_PROTO(ctf_struct_union_get_next_member, struct ctf_struct_union*,
-    struct ctf_member*)
+_CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_name, ctf_struct_union, char*)
+
+_CTF_LISTING_PROTO(ctf_struct_union_get_next_member, ctf_struct_union,
+    ctf_member)
 
 #endif
 
