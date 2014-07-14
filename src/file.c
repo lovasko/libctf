9af14283502b283dd323316a5e46b36d98e60c13
Daniel Lovasko <lovasko@freebsd.org>
2014-07-14 14:46:43 +0200

    File properties and listings implementations made same as the prototypes

--- src/file.c
+++ src/file.c
@@ -1,19 +1,17 @@
 #include "file.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_file_get_version, struct ctf_file*, int, version)
-_CTF_GET_PROPERTY_IMPL(ctf_file_is_compressed, struct ctf_file*, int,
+_CTF_GET_PROPERTY_IMPL(ctf_file_get_version, ctf_file, ctf_version, version)
+_CTF_GET_PROPERTY_IMPL(ctf_file_is_compressed, ctf_file, ctf_bool, 
     is_compressed)
-_CTF_GET_PROPERTY_IMPL(ctf_file_get_parent_file, struct ctf_file*, 
-    struct ctf_file*, parent_file)
-_CTF_GET_PROPERTY_IMPL(ctf_file_get_basename, struct ctf_file*, char*,
-    path_basename)
+_CTF_GET_PROPERTY_IMPL(ctf_file_get_parent_file, ctf_file, ctf_file, 
+    parent_file)
+_CTF_GET_PROPERTY_IMPL(ctf_file_get_basename, ctf_file, char*, path_basename)
 
-_CTF_LISTING_IMPL(ctf_file_get_next_label, struct ctf_file*, struct ctf_label*,
-    label_head, labels);
-_CTF_LISTING_IMPL(ctf_file_get_next_type, struct ctf_file*, struct ctf_type*,
-    type_head, types);
-_CTF_LISTING_IMPL(ctf_file_get_next_function, struct ctf_file*, 
-    struct ctf_function*, function_head, functions);
-_CTF_LISTING_IMPL(ctf_file_get_next_data_object, struct ctf_file*, 
-    struct ctf_data_object*, data_object_head, data_objects);
+_CTF_LISTING_IMPL(ctf_file_get_next_label, ctf_file, ctf_label, label_head,
+    labels)
+_CTF_LISTING_IMPL(ctf_file_get_next_type, ctf_file, ctf_type, type_head, types)
+_CTF_LISTING_IMPL(ctf_file_get_next_function, ctf_file, ctf_function, 
+    function_head, functions);
+_CTF_LISTING_IMPL(ctf_file_get_next_data_object, ctf_file, ctf_data_object,
+    data_object_head, data_objects);
 
