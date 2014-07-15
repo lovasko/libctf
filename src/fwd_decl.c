2f9f1c08b3b6f8437793c6183b6352b04d548a3d
Daniel Lovasko <lovasko@freebsd.org>
2014-07-15 22:44:17 +0200

    Forward declaration intialisation from generic type

--- src/fwd_decl.c
+++ src/fwd_decl.c
@@ -3,3 +3,5 @@
 _CTF_GET_PROPERTY_IMPL(ctf_fwd_decl_get_name, ctf_fwd_decl, char*, name)
 _CTF_GET_PROPERTY_IMPL(ctf_fwd_decl_get_kind, ctf_fwd_decl, ctf_kind, kind)
 
+_CTF_FROM_TYPE_IMPL(ctf_fwd_decl_init, ctf_fwd_decl)
+
--- src/fwd_decl.h
+++ src/fwd_decl.h
@@ -2,6 +2,7 @@
 #define CTF_FWD_DECL_H
 
 #include "kind.h"
+#include "util/from_type.h"
 #include "util/property.h"
 
 #include <stdint.h>
@@ -17,6 +18,8 @@ typedef struct ctf_fwd_decl* ctf_fwd_decl;
 
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_name, ctf_fwd_decl, char*)
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_kind, ctf_fwd_decl, ctf_kind)
+
+_CTF_FROM_TYPE_PROTO(ctf_fwd_decl_init, ctf_fwd_decl)
 
 #endif
 
