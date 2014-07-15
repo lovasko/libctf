faf98976d788cf94e9676f6a8ebd2c52e5e5fcc3
Daniel Lovasko <lovasko@freebsd.org>
2014-07-15 22:44:42 +0200

    Struct/union initialisation from generic type

--- src/struct_union.h
+++ src/struct_union.h
@@ -2,6 +2,7 @@
 #define CTF_STRUCT_UNION_H
 
 #include "member.h"
+#include "util/from_type.h"
 #include "util/property.h"
 #include "util/listing.h"
 
@@ -18,6 +19,8 @@ _CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_name, ctf_struct_union, char*)
 
 _CTF_LISTING_PROTO(ctf_struct_union_get_next_member, ctf_struct_union,
     ctf_member)
+
+_CTF_FROM_TYPE_PROTO(ctf_struct_union_init, ctf_struct_union)
 
 #endif
 
