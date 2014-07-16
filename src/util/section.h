8d9b43b7ff01db940a98190c863b182c94361bcb
Daniel Lovasko <lovasko@freebsd.org>
2014-07-16 16:37:05 +0200

    Documentation update

--- src/util/section.h
+++ src/util/section.h
@@ -5,14 +5,15 @@
 
 /**
  * Simplified representation of universal data section.
+ * 
  * Data sections can be a ELF sections, CTF subsections or any general block of
  * binary data.
  * Designated for internal use only.
  */
 struct _section
 {
-	void *data; /**< section data */
-	size_t size; /**< section size in bytes */
+	void* data; /**< binary data */
+	size_t size; /**< size in bytes */
 };
 #define _SECTION_SIZE sizeof(struct _section)
 
