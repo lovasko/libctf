9ef0cf40a087138e40714fdf4eba90ef79293cdb
Daniel Lovasko <lovasko@freebsd.org>
2014-07-16 18:38:36 +0200

    Finished implementation

--- src/struct_union.c
+++ src/struct_union.c
@@ -6,5 +6,5 @@ _CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_name, ctf_struct_union, char*,
 _CTF_LISTING_IMPL(ctf_struct_union_get_next_member, ctf_struct_union,
     ctf_member, member_head, members)
 
-_CTF_FROM_TYPE_IMPL
+_CTF_FROM_TYPE_IMPL(ctf_struct_union_init, ctf_struct_union)
 
