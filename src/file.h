5e9ff72f98f30ce44357d3117b7421f29c563508
Daniel Lovasko <lovasko@freebsd.org>
2014-07-16 16:45:10 +0200

    Documentation update

--- src/file.h
+++ src/file.h
@@ -24,17 +24,19 @@ struct ctf_file
 {
 	ctf_bool is_compressed; /**< 0/1 false/true flag for the compression */
 	ctf_version version; /**< version indicator */
-	char* path_basename;
+	char* path_basename; /**< basename of the current file containing the CTF
+	    data */
 
-	struct ctf_label_head* label_head; /**< linked list containing labels */
-	struct ctf_type_head* type_head; /**< linked list containing types */
+	struct ctf_label_head* label_head;
+	struct ctf_type_head* type_head;
 	struct ctf_data_object_head* data_object_head;
 	struct ctf_function_head* function_head;
 
-	struct ctf_file* parent_file;
-	uint16_t type_id_offset;
-	struct ctf_type** type_id_table;
-	uint16_t type_count;
+	struct ctf_file* parent_file; /**< pointer to parent file */
+	uint16_t type_id_offset; /**< offset of the type IDs in this file */
+	struct ctf_type** type_id_table; /**< array of ctf_type pointers that is
+	    indexed without the offset */
+	uint16_t type_count; /**< number of types in this CTF file */
 };
 #define CTF_FILE_SIZE sizeof(struct ctf_file)
 
