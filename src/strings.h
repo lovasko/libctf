a45ab8027ccf24cc579f0fc6e989f1bfd5e1316f
Daniel Lovasko <lovasko@freebsd.org>
2014-07-16 14:32:05 +0200

    Documentation

--- src/strings.h
+++ src/strings.h
@@ -5,12 +5,23 @@
 
 #include <stdint.h>
 
+/**
+ * Tuple holding pointers to two string tables: ELF and CTF.
+ */
 struct _strings
 {
 	struct _section* ctf;
 	struct _section* elf;
 };
 
+/**
+ * Look up a string identified by the reference from either ELF or CTF string
+ * table.
+ *
+ * @param strings tuple of string tables
+ * @param reference string reference
+ * @return pointer to NULL-terminated const string 
+ */
 char*
 strings_lookup (struct _strings* strings, uint32_t reference);
 
