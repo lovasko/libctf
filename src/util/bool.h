b062bde4d471c97826aa5ab9221ef49a142743e0
Daniel Lovasko <lovasko@freebsd.org>
2014-07-16 15:05:37 +0200

    Documentation

--- src/util/bool.h
+++ src/util/bool.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
+/*
+ * Since there is no standardized and usable concept of true/false type in
+ * C, we introduced our own type. Normally, boolean type in stdbool.h needs
+ * a whole int type to represent the value, which is a waste. Instead, we 
+ * take up only one byte of memory.
+ */
+
 #define CTF_FALSE 0
 #define CTF_TRUE  1
 
