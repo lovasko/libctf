7cefaa8b2dfc4ac3d46d724beb02923cee9c8e1f
Daniel Lovasko <lovasko@freebsd.org>
2014-07-17 23:25:51 +0200

    Fixed memleak

--- src/read.c
+++ src/read.c
@@ -536,13 +536,21 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	struct _section *ctf_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_SUNW_CTF);
 	if (ctf_section == NULL)
+	{
+		elf_end(elf);
 		return CTF_E_NO_CTF_SECTION;
+	}
 
 	/* find the string table section */
 	struct _section *strtab_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_STRTAB);
 	if (strtab_section == NULL)
+	{
+		free(ctf_section->data);
+		free(ctf_section);
+		elf_end(elf);
 		return CTF_E_NO_STRTAB;
+	}
 
 	/* find the symbol table section */
 	struct _section *symtab_section = elf_section_find(elf, &elf_header,
@@ -576,10 +584,12 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 		if (decompressed == NULL)
 			return CTF_E_COMPRESSION;
 
+		/* FIXME dealloc */
 		if (decompressed->size != header->string_offset + header->string_length)
 			return CTF_E_DECOMPRESSED_DATA_SIZE;
 
 		headerless_ctf = decompressed->data;
+		free(decompressed);
 	}
 	else
 		headerless_ctf = ctf_section->data + _CTF_HEADER_SIZE;
