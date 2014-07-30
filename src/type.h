d9e58ea1f5754b450036eb64583e6142f25b049a
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-30 06:10:30 +0100

    ID boundary constants

--- src/type.h
+++ src/type.h
@@ -63,6 +63,8 @@ struct _ctf_large_type
 #define _CTF_LARGE_TYPE_SIZE sizeof(struct _ctf_large_type)
 
 typedef uint16_t ctf_id;
+#define CTF_ID_MIN     1
+#define CTF_ID_MAX 65536
 
 /**
  * General type information.
