6ac4adda7524d163b44003b96b4df1b69206fb14
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-31 08:05:38 +0100

    Added member count property to struct/union

--- src/struct_union.c
+++ src/struct_union.c
@@ -2,6 +2,8 @@
 
 _CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_name, ctf_struct_union, char*, 
     name)
+_CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_member_count, ctf_struct_union, 
+    ctf_count, member_count)
 
 _CTF_LISTING_IMPL(ctf_struct_union_get_next_member, ctf_struct_union,
     ctf_member, member_head, members)
--- src/struct_union.h
+++ src/struct_union.h
@@ -2,6 +2,7 @@
 #define CTF_STRUCT_UNION_H
 
 #include "member.h"
+#include "count.h"
 #include "util/from_type.h"
 #include "util/property.h"
 #include "util/listing.h"
@@ -9,6 +10,7 @@
 struct ctf_struct_union
 {
 	char* name;
+	ctf_count member_count;
 	struct ctf_member_head* member_head;
 };
 #define CTF_STRUCT_UNION_SIZE sizeof(struct ctf_struct_union)
@@ -16,6 +18,8 @@ struct ctf_struct_union
 typedef struct ctf_struct_union* ctf_struct_union;
 
 _CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_name, ctf_struct_union, char*)
+_CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_member_count, ctf_struct_union, 
+    ctf_count)
 
 _CTF_LISTING_PROTO(ctf_struct_union_get_next_member, ctf_struct_union,
     ctf_member)
