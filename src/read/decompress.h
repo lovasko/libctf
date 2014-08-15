b22faba14649715c57d815278ea1475adf312449
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-15 23:42:24 +0100

    Decompression available only in userspace

--- src/read/decompress.h
+++ src/read/decompress.h
@@ -1,6 +1,8 @@
 #ifndef CTF_READ_DECOMPRESS_H
 #define CTF_READ_DECOMPRESS_H
 
+#ifndef _KERNEL
+
 #include "../util/section.h"
 
 /**
@@ -11,6 +13,8 @@
  */
 struct _section*
 _ctf_decompress (struct _section* to_decompress);
+
+#endif /* _KERNEL */
 
 #endif
 
