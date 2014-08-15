f8f24dd90ff2e7e3176b3a79d7d448751169741b
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-15 19:41:46 +0100

    Usage of the space macros

--- src/read/labels.c
+++ src/read/labels.c
@@ -22,11 +22,7 @@ _ctf_read_labels (struct ctf_file* file, struct _section* section,
 	if (section->size % _CTF_LABEL_SIZE != 0)
 		return CTF_E_LABEL_SECTION_CORRUPT;
 
-#ifdef _KERNEL
-	file->label_head = malloc(CTF_LABEL_HEAD_SIZE, M_CTF, M_WAITOK);
-#elif
-	file->label_head = malloc(CTF_LABEL_HEAD_SIZE);
-#endif
+	file->label_head = CTF_MALLOC(CTF_LABEL_HEAD_SIZE);
 
 	TAILQ_INIT(file->label_head);
 
@@ -34,14 +30,8 @@ _ctf_read_labels (struct ctf_file* file, struct _section* section,
 
 	for (unsigned int i = 0; i < section->size/_CTF_LABEL_SIZE; i++)
 	{
-#ifdef _KERNEL
-		struct ctf_label* label = malloc(CTF_LABEL_SIZE, M_CTF, M_WAITOK);
-		label->name = strdup(_ctf_strings_lookup(strings, raw_labels[i].name),
-		    M_CTF);
-#elif
-		struct ctf_label* label = malloc(CTF_LABEL_SIZE);
-		label->name = strdup(_ctf_strings_lookup(strings, raw_labels[i].name));
-#endif
+		struct ctf_label* label = CTF_MALLOC(CTF_LABEL_SIZE);
+		label->name = CTF_STRDUP(_ctf_strings_lookup(strings, raw_labels[i].name));
 
 		label->index = raw_labels[i].index;
 
