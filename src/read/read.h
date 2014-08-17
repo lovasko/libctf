d48f76a0910840b513e85e3f658a8e4f6410320f
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-17 06:38:00 +0100

    New file reading function that operates on raw data

--- src/read/read.h
+++ src/read/read.h
@@ -2,6 +2,25 @@
 #define CTF_READ_READ_H
 
 #include "../file.h"
+#include "../util/section.h"
+
+/**
+ * Parse the raw uncompressed CTF data.
+ *
+ * @param [in] ctf_data uncompressed CTF data (also with header)
+ * @param [in] symbol_table symbol table of the ELF object
+ * @param [in] string_table string table of the ELF object
+ * @param [out] file parsed CTF data
+ */
+int
+ctf_file_read_data (
+    struct _ctf_section* ctf_data, 
+    struct _ctf_section* symbol_table, 
+    struct _ctf_section* string_table,
+    ctf_file* file);
+
+
+#ifndef _KERNEL
 
 /**
  * Parse the file and extract all CTF data.
@@ -12,6 +31,8 @@
  */
 int
 ctf_file_read (const char* filename, ctf_file* out_file);
+
+#endif /* not _KERNEL */
 
 #endif
 
