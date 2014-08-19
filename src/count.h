3b70012ccec02b28cd623f0c47170117dacf2be4
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-19 20:50:14 +0100

    Documentation update

--- src/count.h
+++ src/count.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+/* Type used everywhere where count (number of) some items is relevant. */
 typedef uint32_t ctf_count;
 #define CTF_COUNT_MIN          0
 #define CTF_COUNT_MAX 0xffffffff
