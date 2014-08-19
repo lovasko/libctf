d23da42ee32ce385eb412b7e3660f87a2c0a8860
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-19 11:36:13 +0100

    Pointer asterisk swap

--- src/read/read.c
+++ src/read/read.c
@@ -271,7 +271,7 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	}
 
 	/* load the ELF file */
-	Elf *elf = NULL;
+	Elf* elf = NULL;
 	if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
 	{
 		close(fd);
@@ -279,7 +279,7 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	}
 
 	/* find the CTF section */
-	struct _section *ctf_section = elf_section_find(elf, &elf_header,
+	struct _section* ctf_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_SUNW_CTF);
 	if (ctf_section == NULL)
 	{
@@ -288,7 +288,7 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	}
 
 	/* find the string table section */
-	struct _section *strtab_section = elf_section_find(elf, &elf_header,
+	struct _section* strtab_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_STRTAB);
 	if (strtab_section == NULL)
 	{
@@ -299,7 +299,7 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	}
 
 	/* find the symbol table section */
-	struct _section *symtab_section = elf_section_find(elf, &elf_header,
+	struct _section* symtab_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_SYMTAB);
 	if (symtab_section == NULL)
 		return CTF_E_NO_SYMTAB;
