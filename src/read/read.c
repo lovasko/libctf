78e785f85dae2c4d2d40f02d4652f63932c4a300
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-19 11:37:26 +0100

    Removed unnecessary checks

--- src/read/read.c
+++ src/read/read.c
@@ -281,28 +281,14 @@ ctf_file_read (const char* filename, ctf_file* out_file)
 	/* find the CTF section */
 	struct _section* ctf_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_SUNW_CTF);
-	if (ctf_section == NULL)
-	{
-		elf_end(elf);
-		return CTF_E_NO_CTF_SECTION;
-	}
 
 	/* find the string table section */
 	struct _section* strtab_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_STRTAB);
-	if (strtab_section == NULL)
-	{
-		free(ctf_section->data);
-		free(ctf_section);
-		elf_end(elf);
-		return CTF_E_NO_STRTAB;
-	}
 
 	/* find the symbol table section */
 	struct _section* symtab_section = elf_section_find(elf, &elf_header,
 	    CTF_ELF_SECTION_SYMTAB);
-	if (symtab_section == NULL)
-		return CTF_E_NO_SYMTAB;
 
 	/* we do not need the ELF data anymore */
 	elf_end(elf);
