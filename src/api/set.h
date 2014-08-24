c5d65675d90d9b112c1c04d6e7ddf69b26708a90
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-24 23:26:41 +0100

    String property set

--- src/api/set.h
+++ src/api/set.h
@@ -5,6 +5,9 @@
 
 #include <stdlib.h>
 
+/* TODO kernel space strdup */
+
+/* general property setter */
 #define _CTF_PROPERTY_SET_PROTO(FUNCTION_NAME, OBJECT_TYPE, PROPERTY_TYPE) \
 int \
 FUNCTION_NAME (OBJECT_TYPE object, PROPERTY_TYPE new_value);
@@ -18,6 +21,30 @@ FUNCTION_NAME (OBJECT_TYPE object, PROPERTY_TYPE new_value) \
 		return CTF_E_NULL; \
 \
 	object->PROPERTY_NAME = new_value; \
+	return CTF_OK; \
+}
+
+/* string property setter */
+#define _CTF_PROPERTY_SET_STRING_PROTO(FUNCTION_NAME, OBJECT_TYPE) \
+int \
+FUNCTION_NAME (OBJECT_TYPE object, char* new_value);
+
+#define _CTF_PROPERTY_SET_STRING_IMPL(FUNCTION_NAME, OBJECT_TYPE, \
+    PROPERTY_NAME) \
+int \
+FUNCTION_NAME (OBJECT_TYPE object, char* new_value) \
+{ \
+	if (object == NULL) \
+		return CTF_E_NULL; \
+\
+	if (object->PROPERTY_NAME != NULL) \
+		free(object->PROPERTY_NAME); \
+\
+	if (new_value == NULL) \
+		object->PROPERTY_NAME = NULL; \
+	else \
+		object->PROPERTY_NAME = strdup(new_value); \
+\
 	return CTF_OK; \
 }
 
