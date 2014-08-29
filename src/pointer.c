7bba4a28b67743ad2aaf0237755fe101e2abaa61
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-08-29 14:46:39 +0100

    Pointer properties prototypes and implementations

---
+++ src/pointer.c
@@ -0,0 +1,6 @@
+#include "pointer.h"
+
+_CTF_GET_PROPERTY_IMPL(ctf_pointer_get_type, ctf_pointer, ctf_type, type)
+
+_CTF_FROM_TYPE_IMPL(ctf_pointer_init, ctf_pointer)
+
--- src/pointer.h
+++ src/pointer.h
@@ -22,5 +22,9 @@ struct ctf_pointer
 
 typedef struct ctf_pointer* ctf_pointer;
 
+_CTF_GET_PROPERTY_PROTO(ctf_pointer_get_type, ctf_pointer, ctf_type)
+
+_CTF_FROM_TYPE_PROTO(ctf_pointer_init, ctf_pointer)
+
 #endif
 
