a66dc9ce575cb802d6774ac7e10060fdfdf4cd08
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-11-02 16:15:40 +0100

    Installation script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/bash
+
+HEADER_PREFIX=/usr/include
+LIB_PREFIX=/usr/lib
+
+mkdir -vp ${HEADER_PREFIX}/libctf
+mkdir -vp ${HEADER_PREFIX}/libctf/file
+mkdir -vp ${HEADER_PREFIX}/libctf/type
+mkdir -vp ${HEADER_PREFIX}/libctf/io
+mkdir -vp ${HEADER_PREFIX}/libctf/io/read
+mkdir -vp ${HEADER_PREFIX}/libctf/label
+mkdir -vp ${HEADER_PREFIX}/libctf/object
+mkdir -vp ${HEADER_PREFIX}/libctf/object/function
+mkdir -vp ${HEADER_PREFIX}/libctf/object/data
+mkdir -vp ${HEADER_PREFIX}/libctf/util
+
+
+for header in `find src/ -name '*.h'`
+do
+	relative=${header/src\//}
+	cp -v src/${relative} ${HEADER_PREFIX}/libctf/${relative}
+done
+
+cp -v bin/libbsdctf.so ${LIB_PREFIX} 
