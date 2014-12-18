385fc41c0ffd7b71b49ff412ee764ffe1f3eedde
Daniel Lovasko <lovasko@freebsd.org>
2014-12-18 11:33:32 +0100

    Int API

--- src/type/int.c
+++ src/type/int.c
@@ -1,16 +1,73 @@
 #include "type/int.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_name, ctf_int, char*, name)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_size, ctf_int, ctf_int_size, size)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_offset, ctf_int, ctf_int_offset, offset)
-_CTF_GET_PROPERTY_IMPL(ctf_int_get_content, ctf_int, ctf_int_content, content)
-_CTF_GET_PROPERTY_IMPL(ctf_int_is_signed, ctf_int, ctf_bool, is_signed)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_int_get_name,
+	ctf_int,
+	char*,
+	name)
 
-_CTF_SET_PROPERTY_IMPL(ctf_int_set_name, ctf_int, char*, name)
+_CTF_SET_PROPERTY_IMPL(
+	ctf_int_set_name,
+	ctf_int,
+	char*,
+	name)
 
-_CTF_CREATE_IMPL(ctf_int_create, ctf_int, CTF_INT_SIZE)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_int_get_size,
+	ctf_int,
+	ctf_int_size,
+	size)
 
-_CTF_FROM_TYPE_IMPL(ctf_int_init, ctf_int)
+_CTF_SET_PROPERTY_IMPL(
+	ctf_int_set_size,
+	ctf_int,
+	ctf_int_size,
+	size)
+
+_CTF_GET_PROPERTY_IMPL(
+	ctf_int_get_offset,
+	ctf_int,
+	ctf_int_offset,
+	offset)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_int_set_offset,
+	ctf_int,
+	ctf_int_offset,
+	offset)
+
+_CTF_GET_PROPERTY_IMPL(
+	ctf_int_get_content,
+	ctf_int,
+	ctf_int_content,
+	content)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_int_set_content,
+	ctf_int,
+	ctf_int_content,
+	content)
+
+_CTF_GET_PROPERTY_IMPL(
+	ctf_int_get_is_signed,
+	ctf_int,
+	uint8_t,
+	is_signed)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_int_set_is_signed,
+	ctf_int,
+	uint8_t,
+	is_signed)
+
+_CTF_FROM_TYPE_IMPL(
+	ctf_int_init,
+	ctf_int)
+
+_CTF_CREATE_IMPL(
+	ctf_int_create,
+	ctf_int,
+	CTF_INT_SIZE)
 
 const char*
 ctf_int_content_to_string (ctf_int_content int_content)
--- src/type/int.h
+++ src/type/int.h
@@ -31,23 +31,29 @@ struct ctf_int
 	ctf_int_content content;
 	ctf_int_offset offset;
 	ctf_int_size size;
-	ctf_bool is_signed;
+	uint8_t is_signed;
 };
 #define CTF_INT_SIZE sizeof(struct ctf_int)
 
 typedef struct ctf_int* ctf_int;
 
 _CTF_GET_PROPERTY_PROTO(ctf_int_get_name, ctf_int, char*)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_size, ctf_int, ctf_int_size)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_offset, ctf_int, ctf_int_offset)
-_CTF_GET_PROPERTY_PROTO(ctf_int_get_content, ctf_int, ctf_int_content)
-_CTF_GET_PROPERTY_PROTO(ctf_int_is_signed, ctf_int, ctf_bool)
-
 _CTF_SET_PROPERTY_PROTO(ctf_int_set_name, ctf_int, char*)
 
-_CTF_CREATE_PROTO(ctf_int_create, ctf_int)
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_size, ctf_int, ctf_int_size)
+_CTF_SET_PROPERTY_PROTO(ctf_int_set_size, ctf_int, ctf_int_size)
+
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_offset, ctf_int, ctf_int_offset)
+_CTF_SET_PROPERTY_PROTO(ctf_int_set_offset, ctf_int, ctf_int_offset)
+
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_content, ctf_int, ctf_int_content)
+_CTF_SET_PROPERTY_PROTO(ctf_int_set_content, ctf_int, ctf_int_content)
+
+_CTF_GET_PROPERTY_PROTO(ctf_int_get_is_signed, ctf_int, uint8_t)
+_CTF_SET_PROPERTY_PROTO(ctf_int_set_is_signed, ctf_int, uint8_t)
 
 _CTF_FROM_TYPE_PROTO(ctf_int_init, ctf_int)
+_CTF_CREATE_PROTO(ctf_int_create, ctf_int)
 
 const char*
 ctf_int_content_to_string(ctf_int_content int_content);
