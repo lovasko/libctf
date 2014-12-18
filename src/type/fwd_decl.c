24ecea79ad16ca8e227d4b966817ebdc8423e033
Daniel Lovasko <lovasko@freebsd.org>
2014-12-18 10:58:48 +0100

    Forward declaration API

--- src/type/fwd_decl.c
+++ src/type/fwd_decl.c
@@ -1,7 +1,35 @@
 #include "type/fwd_decl.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_fwd_decl_get_name, ctf_fwd_decl, char*, name)
-_CTF_GET_PROPERTY_IMPL(ctf_fwd_decl_get_kind, ctf_fwd_decl, ctf_kind, kind)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_fwd_decl_get_name,
+	ctf_fwd_decl,
+	char*,
+	name)
 
-_CTF_FROM_TYPE_IMPL(ctf_fwd_decl_init, ctf_fwd_decl)
+_CTF_SET_PROPERTY_IMPL(
+	ctf_fwd_decl_set_name,
+	ctf_fwd_decl,
+	char*,
+	name)
+
+_CTF_GET_PROPERTY_IMPL(
+	ctf_fwd_decl_get_kind,
+	ctf_fwd_decl,
+	ctf_kind,
+	kind)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_fwd_decl_set_kind,
+	ctf_fwd_decl,
+	ctf_kind,
+	kind)
+
+_CTF_FROM_TYPE_IMPL(
+	ctf_fwd_decl_init,
+	ctf_fwd_decl)
+
+_CTF_CREATE_IMPL(
+	ctf_fwd_decl_create,
+	ctf_fwd_decl,
+	CTF_FWD_DECL_SIZE)
 
--- src/type/fwd_decl.h
+++ src/type/fwd_decl.h
@@ -4,6 +4,7 @@
 #include "type/kind.h"
 #include "util/from_type.h"
 #include "util/property.h"
+#include "util/create.h"
 
 #include <stdint.h>
 
@@ -20,9 +21,13 @@ struct ctf_fwd_decl
 typedef struct ctf_fwd_decl* ctf_fwd_decl;
 
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_name, ctf_fwd_decl, char*)
+_CTF_SET_PROPERTY_PROTO(ctf_fwd_decl_set_name, ctf_fwd_decl, char*)
+
 _CTF_GET_PROPERTY_PROTO(ctf_fwd_decl_get_kind, ctf_fwd_decl, ctf_kind)
+_CTF_SET_PROPERTY_PROTO(ctf_fwd_decl_set_kind, ctf_fwd_decl, ctf_kind)
 
 _CTF_FROM_TYPE_PROTO(ctf_fwd_decl_init, ctf_fwd_decl)
+_CTF_CREATE_PROTO(ctf_fwd_decl_create, ctf_fwd_decl)
 
 #endif
 
