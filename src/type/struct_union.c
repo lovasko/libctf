6f720e696d359b0e6fdea518f4839f42798ce8c6
Daniel Lovasko <lovasko@freebsd.org>
2014-12-18 12:23:41 +0100

    Struct/union API

--- src/type/struct_union.c
+++ src/type/struct_union.c
@@ -1,12 +1,36 @@
 #include "type/struct_union.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_name, ctf_struct_union, char*, 
-    name)
-_CTF_GET_PROPERTY_IMPL(ctf_struct_union_get_member_count, ctf_struct_union, 
-    ctf_count, member_count)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_struct_union_get_name,
+	ctf_struct_union,
+	char*,
+	name)
 
-_CTF_LISTING_IMPL(ctf_struct_union_get_next_member, ctf_struct_union,
-    ctf_member, member_head, members)
+_CTF_SET_PROPERTY_IMPL(
+	ctf_struct_union_set_name,
+	ctf_struct_union,
+	char*,
+	name)
 
-_CTF_FROM_TYPE_IMPL(ctf_struct_union_init, ctf_struct_union)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_struct_union_get_member_count,
+	ctf_struct_union,
+	ctf_count,
+	member_count)
+
+_CTF_LISTING_IMPL(
+	ctf_struct_union_get_next_member, 
+	ctf_struct_union,
+	ctf_member,
+	member_head,
+	members)
+
+_CTF_FROM_TYPE_IMPL(
+	ctf_struct_union_init,
+	ctf_struct_union)
+
+_CTF_CREATE_IMPL(
+	ctf_struct_union_create,
+	ctf_struct_union,
+	CTF_STRUCT_UNION_SIZE)
 
--- src/type/struct_union.h
+++ src/type/struct_union.h
@@ -6,6 +6,7 @@
 #include "util/from_type.h"
 #include "util/property.h"
 #include "util/listing.h"
+#include "util/create.h"
 
 struct ctf_struct_union
 {
@@ -18,13 +19,15 @@ struct ctf_struct_union
 typedef struct ctf_struct_union* ctf_struct_union;
 
 _CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_name, ctf_struct_union, char*)
+_CTF_SET_PROPERTY_PROTO(ctf_struct_union_set_name, ctf_struct_union, char*)
+
 _CTF_GET_PROPERTY_PROTO(ctf_struct_union_get_member_count, ctf_struct_union, 
     ctf_count)
-
 _CTF_LISTING_PROTO(ctf_struct_union_get_next_member, ctf_struct_union,
     ctf_member)
 
 _CTF_FROM_TYPE_PROTO(ctf_struct_union_init, ctf_struct_union)
+_CTF_CREATE_PROTO(ctf_struct_union_create, ctf_struct_union)
 
 #endif
 
