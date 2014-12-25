85e56ba62fa0e0171c4a6a6f908fd505628a1ea6
Daniel Lovasko <lovasko@freebsd.org>
2014-12-25 07:25:43 +0100

    Initial add() API

---
+++ src/api/add.h
@@ -0,0 +1,33 @@
+#ifndef CTF_ADD_H
+#define CTF_ADD_H
+
+#include <sys/queue.h>
+#include <sys/stddef.h>
+
+#include "file/errors.h"
+
+#define _CTF_ADD_PROTO(FUNCTION_NAME, OBJECT_TYPE, VALUE_TYPE) \
+int \
+FUNCTION_NAME (OBJECT_TYPE object, VALUE_TYPE value);
+
+#define _CTF_ADD_IMPL(FUNCTION_NAME, OBJECT_TYPE, VALUE_TYPE, LIST_NAME, \
+  LIST_ENTRY_NAME) \
+int \
+FUNCTION_NAME (OBJECT_TYPE object, VALUE_TYPE value) \
+{ \
+	if (object != NULL) \
+	{ \
+		if (object->LIST_NAME != NULL) \
+		{ \
+			TAILQ_INSERT_TAIL(object->LIST_NAME, value, LIST_ENTRY_NAME); \
+			return CTF_OK; \
+		} \
+		else \
+			return CTF_E_NULL; \
+	} \
+	else \
+		return CTF_E_NULL; \
+}
+
+#endif
+
