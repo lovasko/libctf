b2348cbde1453c566e6016ed31cbcaa16133c249
Daniel Lovasko <lovasko@freebsd.org>
2015-01-03 22:15:56 +0100

    Memory usage API macro

---
+++ src/api/memory_usage.h
@@ -0,0 +1,11 @@
+#ifndef CTF_MEMORY_USAGE_H
+#define CTF_MEMORU_USAGE_H
+
+#include <sys/stddef.h>
+
+#define _CTF_MEMORY_USAGE_PROTO(FUNCTION_NAME, OBJECT_TYPE) \
+size_t \
+FUNCTION_NAME (OBJECT_TYPE object);
+
+#endif
+
