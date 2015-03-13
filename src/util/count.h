8c66414dcb4952edd0eb47ff6ec6afb9770206b4
Daniel Lovasko <lovasko@freebsd.org>
2015-03-13 16:06:26 +0100

    Cleaned up includes

--- src/util/count.h
+++ src/util/count.h
@@ -1,7 +1,7 @@
 #ifndef CTF_COUNT_H
 #define CTF_COUNT_H
 
-#include <stdint.h>
+#include <sys/stdint.h>
 
 /* Type used everywhere where count (number of) some items is relevant. */
 typedef uint32_t ctf_count;
