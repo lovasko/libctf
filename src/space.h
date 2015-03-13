46f928dfd453a32a33e469b0b10e9dff92e0f161
Daniel Lovasko <lovasko@freebsd.org>
2015-03-13 16:09:38 +0100

    Moved space related macros to top level

--- src/space.h
+++ src/space.h
@@ -11,14 +11,14 @@
 #ifdef _KERNEL
 
 	#define CTF_MALLOC(nbytes) malloc(nbytes, M_CTF, M_WAITOK)
-	#define CTF_MALLOC_DECLARE(name) MALLOC_DECLARE(name)
+	#define CTF_MALLOC_DECLARE() MALLOC_DECLARE(M_CTF)
 	#define CTF_STRDUP(string) strdup(string, M_CTF)
 	#define CTF_FREE(ptr) free(ptr, M_CTF)
 
 #else
 
 	#define CTF_MALLOC(nbytes) malloc(nbytes)
-	#define CTF_MALLOC_DECLARE(name) 
+	#define CTF_MALLOC_DECLARE() 
 	#define CTF_STRDUP(string) strdup(string)
 	#define CTF_FREE(ptr) free(ptr)
 
