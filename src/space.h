3e0c9917ae1f084491b049d7b37d82482bb83c7a
Daniel Lovasko <lovasko@freebsd.org>
2015-03-13 16:13:23 +0100

    Added space-relative includes

--- src/space.h
+++ src/space.h
@@ -10,12 +10,22 @@
 
 #ifdef _KERNEL
 
+	#include <sys/libkern.h>
+	#include <sys/malloc.h>
+	#include <sys/systm.h>
+	#include <sys/kernel.h>
+
 	#define CTF_MALLOC(nbytes) malloc(nbytes, M_CTF, M_WAITOK)
 	#define CTF_MALLOC_DECLARE() MALLOC_DECLARE(M_CTF)
 	#define CTF_STRDUP(string) strdup(string, M_CTF)
 	#define CTF_FREE(ptr) free(ptr, M_CTF)
 
 #else
+
+	#include <stdlib.h>
+	#include <unistd.h>
+	#include <stdio.h>
+	#include <string.h>
 
 	#define CTF_MALLOC(nbytes) malloc(nbytes)
 	#define CTF_MALLOC_DECLARE() 
