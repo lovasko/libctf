0dfe8ebc5d995fb8b0272f6563ceaba0d141a3de
Daniel Lovasko <lovasko@freebsd.org>
2015-05-02 19:17:56 +0100

    More customizable installation

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 HEADER_PREFIX=/usr/include
 LIB_PREFIX=/usr/lib
+CTF_DIR=ctf
 
-mkdir -vp ${HEADER_PREFIX}/libctf
-mkdir -vp ${HEADER_PREFIX}/libctf/api
-mkdir -vp ${HEADER_PREFIX}/libctf/file
-mkdir -vp ${HEADER_PREFIX}/libctf/type
-mkdir -vp ${HEADER_PREFIX}/libctf/io
-mkdir -vp ${HEADER_PREFIX}/libctf/io/read
-mkdir -vp ${HEADER_PREFIX}/libctf/label
-mkdir -vp ${HEADER_PREFIX}/libctf/object
-mkdir -vp ${HEADER_PREFIX}/libctf/object/function
-mkdir -vp ${HEADER_PREFIX}/libctf/object/data
-mkdir -vp ${HEADER_PREFIX}/libctf/util
-
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/api
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/file
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/type
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/io
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/io/read
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/label
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/object
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/object/function
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/object/data
+mkdir -vp ${HEADER_PREFIX}/${CTF_DIR}/util
 
 for header in `find src/ -name '*.h'`
 do
 	relative=${header/src\//}
-	cp -v src/${relative} ${HEADER_PREFIX}/libctf/${relative}
+	cp -v src/${relative} ${HEADER_PREFIX}/${CTF_DIR}/${relative}
 done
 
 cp -v bin/libbsdctf.so ${LIB_PREFIX} 
