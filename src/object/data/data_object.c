08e1cc7a97d7299ea577770b386bf675376073ea
Daniel Lovasko <lovasko@freebsd.org>
2015-05-06 01:48:03 +0100

    symbol fields for data object

--- src/object/data/data_object.c
+++ src/object/data/data_object.c
@@ -1,6 +1,26 @@
 #include "data_object.h"
 
-_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_name, ctf_data_object, char*, name)
-_CTF_GET_PROPERTY_IMPL(ctf_data_object_get_type, ctf_data_object, ctf_type, 
-    type)
+_CTF_GET_PROPERTY_IMPL(
+	ctf_data_object_get_name, 
+	ctf_data_object, 
+	char*, 
+	name)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_data_object_set_name, 
+	ctf_data_object, 
+	char*, 
+	name)
+
+_CTF_GET_PROPERTY_IMPL(
+	ctf_data_object_get_type, 
+	ctf_data_object, 
+	ctf_type, 
+	type)
+
+_CTF_SET_PROPERTY_IMPL(
+	ctf_data_object_set_type, 
+	ctf_data_object, 
+	ctf_type, 
+	type)
 
--- src/object/data/data_object.h
+++ src/object/data/data_object.h
@@ -1,10 +1,10 @@
 #ifndef CTF_DATA_OBJECT_H
 #define CTF_DATA_OBJECT_H
 
-#include "type/type.h"
 #include "api/property.h"
+#include "type/type.h"
 
-#include <stdint.h>
+#include <sys/stdint.h>
 #include <sys/queue.h>
 
 /**
@@ -17,6 +17,9 @@ struct ctf_data_object
 	char* name; /*< name of the data object */
 	ctf_type type; /*< type of the data object */
 
+	uint32_t value;
+	uint32_t size;
+
 	TAILQ_ENTRY(ctf_data_object) data_objects;
 };
 #define CTF_DATA_OBJECT_SIZE sizeof(struct ctf_data_object)
@@ -27,7 +30,10 @@ TAILQ_HEAD(ctf_data_object_head, ctf_data_object);
 #define CTF_DATA_OBJECT_HEAD_SIZE sizeof(struct ctf_data_object_head)
 
 _CTF_GET_PROPERTY_PROTO(ctf_data_object_get_name, ctf_data_object, char*)
+_CTF_SET_PROPERTY_PROTO(ctf_data_object_set_name, ctf_data_object, char*)
+
 _CTF_GET_PROPERTY_PROTO(ctf_data_object_get_type, ctf_data_object, ctf_type)
+_CTF_SET_PROPERTY_PROTO(ctf_data_object_set_type, ctf_data_object, ctf_type)
 
 #endif
 
