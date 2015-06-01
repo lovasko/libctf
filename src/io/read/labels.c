0dbf46b881aac4299bb98c6c71e070b70ad176dc
Daniel Lovasko <lovasko@freebsd.org>
2015-06-01 07:01:16 +0100

    move towards C89

--- src/io/read/labels.c
+++ src/io/read/labels.c
@@ -1,5 +1,3 @@
-/* #include <sys/queue.h> */
-
 #ifdef _KERNEL
 	#include <sys/malloc.h>
 	#include <sys/libkern.h>
@@ -20,14 +18,18 @@ _ctf_read_labels(struct ctf_file* file,
                  struct _section* section, 
                  struct _strings* strings)
 {
+	struct _ctf_label* raw_labels;
+	struct ctf_label* label;
+	unsigned int i;
+
 	if (section->size % _CTF_LABEL_SIZE != 0)
 		return CTF_E_LABEL_SECTION_CORRUPT;
 
 	m_list_init(&file->labels);
-	struct _ctf_label* raw_labels = (struct _ctf_label*)section->data;
-	for (unsigned int i = 0; i < section->size/_CTF_LABEL_SIZE; i++)
+	raw_labels = (struct _ctf_label*)section->data;
+	for (i = 0; i < section->size/_CTF_LABEL_SIZE; i++)
 	{
-		struct ctf_label* label = CTF_MALLOC(CTF_LABEL_SIZE);
+		label = CTF_MALLOC(CTF_LABEL_SIZE);
 		label->name = CTF_STRDUP(_ctf_strings_lookup(strings, raw_labels[i].name));
 		label->index = raw_labels[i].index;
 
